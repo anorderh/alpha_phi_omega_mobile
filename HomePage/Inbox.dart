dbfffd468c28941a5dddfe5754b1ba44b6339a6c
Anthony NorderhendqCDcd <anthony@norderhaug.org>
2022-03-29 17:55:05 -0700

    inbox & sent work

--- HomePage/Inbox.dart
+++ HomePage/Inbox.dart
@@ -16,12 +16,16 @@ class Inbox extends StatefulWidget {
 class _InboxState extends State<Inbox> {
   late List<Widget> pages;
   PageController controller = PageController(initialPage: 0);
+  late bool headerVisible;
+  late String header;
   late Widget loadBox;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    header = 'Inbox';
+    headerVisible = true;
     loadBox = const SizedBox.shrink();
   }
 
@@ -39,20 +43,59 @@ class _InboxState extends State<Inbox> {
     });
   }
 
+  void pageChange(int index) {
+    setState(() {
+      if (index == 1) {
+        header = 'Sent';
+      } else {
+        header = 'Inbox';
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 5000,
+        width: 5000,
         height: 5000,
         padding: const EdgeInsets.all(5),
         child: Stack(
+          alignment: Alignment.center,
           children: <Widget>[
             PageView(
+                onPageChanged: pageChange,
                 controller: controller,
                 children: [
-                  MailList(mail: widget.received, scrollPhysics: const NeverScrollableScrollPhysics()),
-                  MailList(mail: widget.sent, scrollPhysics: const NeverScrollableScrollPhysics())
-                ]
+                  MailList(
+                      mail: widget.received,
+                      scrollPhysics: const NeverScrollableScrollPhysics()),
+                  MailList(
+                      mail: widget.sent,
+                      scrollPhysics: const NeverScrollableScrollPhysics())
+                ]),
+            Align(
+              alignment: Alignment.topRight,
+              child: AnimatedOpacity(
+                  opacity: headerVisible ? 1.0 : 1.0,
+                  duration: const Duration(milliseconds: 500),
+                  child: Container(
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.blueAccent,
+                      ),
+                      child: FittedBox(
+                          fit: BoxFit.contain,
+                          child: Padding(
+                            padding: EdgeInsets.all(5),
+                            child: Text(
+                              header,
+                              style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          )))),
             ),
             loadBox
           ],
